a2d46ef59fe681c58940b0ffb8bfa9b7cf45e687
Hai Hoang nguyen <hoangngn.26@gmail.com>
2023-01-31 16:06:02 +0700

    3 queries completed

--- queries.sql
+++ queries.sql
@@ -1,1 +1,16 @@
--- Task 1: 
+USE resort_db;
+
+-- Task 1: Print all the details (ID, Name, Cost) of the villa. Note that the cost must be prefixed with a '$' sign. Sort the records in order of price with the most expensive villa at the top of the list.
+SELECT VillaID, VillaName, concat('$', VillaCostPerDay) 'VillaCostPerDay'
+FROM Villa
+ORDER BY VillaCostPerDay DESC;
+
+-- Task 2: print the reservation details (VillaID, check in and out dates, along with the number of days) that each of the villas in the reservation are reserved for.
+SELECT VillaID, DateFrom, DateTo, DATEDIFF(DateTo, DateFrom) 'Length of stay'
+FROM Villa_Reservation; 
+
+-- Task 3: print the all details (Name, Cost) of the villa if they are two-bedroom villas.
+SELECT V.VillaName, V.VillaCostPerDay
+FROM VillaType VT, Villa V
+WHERE V.VillaID = VT.VillaID
+AND VT.VillaTypeName like '%two-bedroom%';
